4cf97533f5b160e1f1f997d1a7c01fdcaef0dac2
ToniPopov <tonitheonea@gmail.com>
2017-08-30 13:57:42 +0300

    a

--- src/Dog.java
+++ src/Dog.java
@@ -7,12 +7,6 @@ public class Dog extends Animal {
 	void makeSomeNoise() {
 		System.out.println("bau bau");
 	}
-	// Animal debug() { return this; }
-	// Animal debug() { return this; }
-	// Object debug() { return this; }
-	// int debug() { return 1; }
-	// int debug(int x) { return 1; }
-	// Object debug (int x) { return this; }
 
 	@Override
 	void show() {
@@ -21,6 +15,7 @@ public class Dog extends Animal {
 	public static void main(String[] args){
 		Dog dog = new Dog();
 		Animal l = dog.debug();
+		dog.makeSomeNoise();
 		l.d();
 	}
 
