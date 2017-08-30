2748aafcc659731026b6d3bb418a7556f7609678
ToniPopov <tonitheonea@gmail.com>
2017-08-30 14:18:07 +0300

    main

--- src/Dog.java
+++ src/Dog.java
@@ -12,13 +12,13 @@ public class Dog extends Animal {
 	void show() {
 		System.out.println("kucheto");
 	}
-	public static void main(String[] args){
-		Dog dog = new Dog();
-		Animal l = dog.debug();
-		dog.makeSomeNoise();
-		l.d();
-		l.name = "asd";
-		
-	}
+//	public static void main(String[] args){
+//		Dog dog = new Dog();
+//		Animal l = dog.debug();
+//		dog.makeSomeNoise();
+//		l.d();
+//		l.name = "asd";
+//		
+//	}
 
 }
